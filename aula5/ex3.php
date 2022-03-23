b730e1a429c3ee7df383374a40349e004cc0ce80
renan <renancastroandrade31@gmail.com>
2022-03-23 11:04:20 -0300

    switch

---
+++ aula5/ex3.php
@@ -0,0 +1,14 @@
+<?php
+    $op = 'a';
+    switch($op)
+    {
+        case 'a': echo "Abrir";
+        break;
+        case 'f': echo "Fechar";
+        break;
+        case 's': echo "Salvar";
+        break;
+        default: echo "Sair";
+        break;
+    }
+?>
