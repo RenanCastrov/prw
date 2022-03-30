b0fc8fbe24f43b7b92998ab36e871c2e363e5625
renan <renancastroandrade31@gmail.com>
2022-03-30 10:28:07 -0300

    exemplos funções php

---
+++ aula6/exemplotrim.php
@@ -0,0 +1,6 @@
+<?php
+    $palavra = "  Ciência da Computação ";
+    echo "A variável contém: ".strlen($palavra)." caracteres"."<br>";
+    $palavra = trim($palavra);
+    echo "A variável agora contém ".strlen($palavra)." caracteres"."<br>";
+?>
