738afd5fa90798188cb3b61ae4c2dfdd8b0f6db4
renan <renancastroandrade31@gmail.com>
2022-04-06 09:43:54 -0300

    exemplin

---
+++ aula7/exemplo1.php
@@ -0,0 +1,7 @@
+<?php
+    $alunos = array('José', 'Joao', 'Pedro', 'Maria');
+    for($x = 0; $x <= count($alunos); $x++)
+    {
+        echo "Aluno: ".$x." || Nome: ".$alunos[$x]."<br>";
+    }
+?>
