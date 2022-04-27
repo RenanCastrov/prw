ad63813d677ca64a66b338144a0fdafddaf29707
renan <renancastroandrade31@gmail.com>
2022-04-27 08:46:13 -0300

    ouou

--- projeto01/ednaldo.sql
+++ projeto01/ednaldo.sql
@@ -1,6 +1,6 @@
 use projeto01;
 
-INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+INSERT INTO usuario (nome_usuario, email_usuario, fone_usuario)
 VALUES ('Murilo Sauro', 'murilo@mail.com', '9090-7070');
 
 SELECT * FROM usuario;
